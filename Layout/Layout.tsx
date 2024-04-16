ea5a646368b89baf36afc62fae628b817d020179
KevinVanHA <118561655+KevinVanHA@users.noreply.github.com>
2024-04-16 15:37:26 -0500

    v2bg

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,92 +1,60 @@
-import { Box,  } from "@mui/material";
-import  Head  from "next/head";
+import { Box } from "@mui/material";
+import Head from "next/head";
 import { useState } from "react";
 import Navbar from "../src/components/Navbar/Navbar";
 import { ILayout } from "../src/Types/Types";
 import CustomDrawer from "../src/components/Drawer/Drawer";
 import Footer from "../src/components/Footer/Footer";
 
+const Layout = ({ navbarSx, title, children, desc }: ILayout) => {
+  const [isOpen, setOpen] = useState(false);
 
-
-const Layout = ({navbarSx, title ,children ,desc } : ILayout) => {
-  const [isOpen,setOpen] = useState(false)
-    const toggleDrawer = (state?:boolean) => {
-      setOpen(state !== undefined ? state : !isOpen)
-    }
+  const toggleDrawer = (state?: boolean) => {
+    setOpen(state !== undefined ? state : !isOpen);
+  };
 
   return (
-
     <>
-    <Head >
+      <Head>
         <title>{title || "injoy Holographic NFT"}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
         <link rel="shortcut icon" href="../public/assets/favicon.png" />
-  
-   
-    
-<meta name="description" content={`${desc || 'injoi Holographic NFT'}`} />
 
+        <meta name="description" content={`${desc || "injoi Holographic NFT"}`} />
 
-<meta property="og:title" content="injoi Holographic NFT" />
+        <meta property="og:title" content="injoi Holographic NFT" />
 
+        <meta property="og:url" content="https://injoi.xyz" />
 
-<meta property="og:url" content="https://injoi.xyz" />
+        <meta
+          property="og:description"
+          content="injoi - a free NFT mint, a digital muse, and a gateway to rewards "
+        />
 
+        <meta property="og:image" content="../public/assets/injoilogo.png" />
 
-<meta property="og:description" content="injoi - a free NFT mint, a digital muse, and a gateway to rewards "/>
+        <meta httpEquiv="content-language" content="en" />
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Navbar toggleDrawer={toggleDrawer} navbarSx={navbarSx} />
+      <CustomDrawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
+      <Box
+        className="site-content"
+        sx={{
+          width: "100vw", // Set the width of the content to 100vw
+          overflowX: "hidden", // Prevent horizontal scrollbar
+        }}
+      >
+        {children}
+      </Box>
+      <Footer />
+    </>
+  );
+};
 
-
-<meta property="og:image" content="../public/assets/injoilogo.png"/>
-
-{/* <!-- Og Type -->
-<meta property="og:type" content="article" /> */}
-   
-   
-   <meta httpEquiv="content-language" content="en"/>
-    <meta charSet="UTF-8"/>
-    <meta name="robots" content="index, follow"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    {/* <meta name="description" content="Site Meta Description" /> */}
-    <meta name="keywords" content="Personal Portfolio" />
-    <meta name="author" content="Vito Medlej" />
-    <meta name="publisher" content="publisher"/>
-    <meta name="copyright" content="copyright"/>
-    <meta name="page-topic" content="Web Development | Blog"/>
-    <meta name="page-type" content="Blogging"/>
-    <meta name="audience" content="Everyone"/>
-    {/* <!-- Facebook, whatsapp, instagram, twitter and other popular social media --> */}
-    {/* <meta property="og:title" content={title}/> */}
-    {/* <meta property="og:description" content="Site Content short description"/> */}
-    {/* <meta property="og:image" content="http://example.com/thumbnail.jpg"/> */}
-    {/* <meta property="og:url" content="http://example.com"/> */}
-    {/* <meta name="twitter:card" content="summary_large_image"/> */}
-
-    {/* <!-- Some Non-essential but recommended --> */}
-    {/* <meta property="og:site_name" content="Website Name"> <!-- Optional --> */}
-    {/* <meta name="twitter:image:alt" content="Alt text for image"> <!-- Optional --> */}
-
-    {/* <!-- If has need analytics for facebook and twitter --> */}
-    {/* <meta property="fb:app_id" content="your_app_id" /> */}
-    {/* <meta name="twitter:site" content="@website-username"/> */}
-   
-  
-    </Head> 
-    <Navbar toggleDrawer={toggleDrawer} navbarSx={navbarSx}/>
-    <CustomDrawer isOpen={isOpen} toggleDrawer={toggleDrawer}/>
-    <Box 
-     className="site-content">
-      {children}
-    </Box>
-    <Footer />
-  </>
-  )
-
-
-}
- 
- 
-  
-
-
-
-export default Layout; 
+export default Layout;
--- src/components/Sections/Hero/Hero.tsx
+++ src/components/Sections/Hero/Hero.tsx
@@ -1,15 +1,16 @@
-import {Box, Button, Container, Grid, Tooltip, Typography} from '@mui/material';
+import { Box, Button, Container, Grid, Typography } from '@mui/material';
 import gsap from 'gsap';
-import {useRef, useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
 import HeroSectionAnimation from '../../../gsap/HeroSectionAnimation';
-import Image from 'next/image'
-import injoi from '../../../../public/assets/injoi.gif'
+import Image from 'next/image';
+import BPHeroimage from '../../../../public/assets/BPHeroimage.png';
 
+// Define button styles
 export const btnStyles = {
     padding: '.77em 1.5em',
     borderRadius: '3px'
-}
+};
 
 const Hero = () => {
     const ref = useRef();
@@ -18,230 +19,234 @@ const Hero = () => {
         .selector(ref);
     gsap.registerPlugin(ScrollToPlugin);
     useEffect(() => {
-        HeroSectionAnimation(q)
-
-    }, [q])
+        HeroSectionAnimation(q);
+    }, [q]);
 
     return (
-        <Container
-            id='hero'
-            maxWidth='lg'
-            sx={{
-            margin: '0 auto',
-            py: {
-                xs: '7.5em',
-                sm: '8.5em'
-            }
-        }}>
-            <Grid
-                container
+        <div className="hero-container"> {/* Apply the .hero-container class */}
+            <Container
+                id='hero'
+                
                 sx={{
-                justifyContent: 'space-between'
-            }}>
-
-                <Grid item xs={12} sm={10} md={8} lg={7}>
-                    <Box ref={ref}>
-
-                        <Typography
-                            className='t1'
-                            variant='h1'
-                            sx={{
-                            fontSize: {
-                                xs: '2.4em',
-                                sm: '3.4em',
-                                md: '3.8em'
-                            },
-                            textAlign: 'left',
-                            transform: 'translateY(40px)',
-                            opacity: 0,
-                            pt: '1em',
-                            fontWeight: '600'
-                        }}>
-                      
-                        Meet injoi,  <br />a digital <br /> hologram girl 
-                        </Typography>
-                        <Typography
-                            variant='h2'
-                            className='secondary t2 t25o0'
-                            sx={{
-                            textAlign: 'left',
-                            pt: '1.5em',
-                            fontSize: {
-                                xs: '.9em',
-                                sm: '1em'
-                            },
-                            maxWidth: '570px',
-                            fontWeight: '300'
-                        }}>
-                           Born from the depths of cyberspace, where pixels dance with the rhythm of imagination and dreams take flight in the realm of virtual reality. With her ethereal beauty and enigmatic charm, injoi emerges as a beacon of light in a world shrouded in darkness. 
-                        </Typography>
-
-                        <Box
-    sx={{
-        my: '2.5em',
-        gap: '.8em',
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center'
-    }}>
-    {/* Discord Button */}
-    <a href={`https://discord.com/channels/1131625905601650719/1220060290335379516/1221344390354571324`} rel="noreferrer" target='_blank'>
-        <Button
-            className="b1 explore offset"
-            sx={{
-                opacity: 0,
-                borderRadius:0,
-                padding : '.75em 2.5em',
-                flex:{xs:1,sm:'inherit'},
-            }}
-        >
-            Join for Whitelist
-        </Button>
-    </a>
-
-    {/* Opensea Button */}
-    <a href={`https://opensea.io/`} rel="noreferrer" target='_blank'>
-        <Button
-            className='b2'
-            sx={{
-                ...btnStyles,
-                opacity: 0,
-                height: 'max-content',
-                padding: '1em 1.5em',
-                width:{xs:'100%',sm:'auto'},
-                ":hover": {
-                    color: '#0092ff'
-                }
-            }}
-            variant='text'>
-            <Typography fontSize='14px'>
-                View Opensea
-            </Typography>
-        </Button>
-    </a>
-</Box>
-
-                    </Box>
-                </Grid>
-                <Grid
-                    sx={{
-                    mb: {
-                        xs: '3.5em',
-                        sm: '4em',
-                        lg: '1em'
+                    margin: '0 auto',
+                    py: {
+                        xs: '7.5em',
+                        sm: '8.5em'
                     },
-                    mr: {
-                        sm: '1em',
-                        md: '2em',
-                        lg: '0em'
-                    },
-                    display: 'flex',
-                    alignItems: 'baseline',
-                    justifyContent: 'flex-end'
+                    backgroundImage: `url(${BPHeroimage.src})`, // Corrected background image
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    minHeight: '100vh', // Ensures the image fills the entire screen height
+                    width: '100%', // Make the Container full width of its parent
+                    borderRadius: 0, // Remove border-radius
                 }}
-                    item
-                    xs={12}
-                    sm={12}
-                    md={12}
-                    lg={5}>
-                    <Box
-                        sx={{
-                        borderRadius: '6px',
-                        width: {
-                            xs: '100%',
-                            sm: '350px',
-                            md: '400px'
-                        }
-                    }}>
-                        <Box
-                            className='mainBox'
-                            sx={{
-                            opacity: 0,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            position: 'relative',
-                            height: '460px',
-                            boxShadow: {
-                                xs: '.5em 3em 0 #313131 ',
-                                sm: '2em 3em 0px #313131'
-                            }
-                        }}>
-
-                            <Image
-
-                            
-                            layout='fill'
-
-                                style={{
-                                opacity: 0,
-                                borderRadius: '6px',
-                                zIndex: '2'
-                            }}
-                                className='img1'
-                                src={`/assets/injoi.gif`}
-                                alt="injoy gif"/>
-                            <Box
-                                className='gradientBg'
+            >
+                <Grid
+                    container
+                    sx={{
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <Grid item xs={12} sm={10} md={8} lg={7}>
+                        <Box ref={ref}>
+                            {/* Your content here */}
+                            <Typography
+                                className='t1'
+                                variant='h1'
                                 sx={{
-                                width: '100px',
-                                height: '100px',
-                                zIndex: '0',
-                                position: 'absolute',
-                                right: {
-                                    xs: '-1%',
-                                    sm: '-5%'
-                                },
-                                opacity: 0,
-                                bottom: '-5%',
-                                background: 'transparent',
-                                backgroundImage: 'radial-gradient(white 2px, transparent 0)',
-                                backgroundSize: '15px 13px'
-                            }}></Box>
-                            <Box
-                                className='quoteBox'
+                                    fontSize: {
+                                        xs: '2.4em',
+                                        sm: '3.4em',
+                                        md: '3.8em'
+                                    },
+                                    textAlign: 'left',
+                                    transform: 'translateY(40px)',
+                                    opacity: 0,
+                                    pt: '1em',
+                                    fontWeight: '600'
+                                }}
+                            >
+                                Meet injoi,  <br />a digital <br /> hologram girl
+                            </Typography>
+                            <Typography
+                                variant='h2'
+                                className='secondary t2 t25o0'
                                 sx={{
-                                zIndex: '2',
-                                position: 'absolute',
-                                bottom: {
-                                    xs: '0%',
-                                    lg: '-5%'
-                                },
-                                width: '100%',
-                                right: {
-                                    sm: '25%'
-                                },
-                                top: '105%',
-                                overflow: 'hidden',
-                                opacity: 0,
-                                background: '#0092ff'
-                            }}>
-                                <Typography
-                                    sx={{
-                                    fontWeight: '300',
-                                    fontSize: '.85em',
-                                    padding: '1em'
-                                }}>
-{"A free NFT mint, a digital muse, and a gateway to rewards "}
-    
-    <span style={{ color: 'white' }}>
-        {" - By "}
-        <a href="https://eacnft.com/" target="_blank" rel="noopener noreferrer">
-            {"EAC"}
-        </a>
-    </span>
-                                </Typography>
+                                    textAlign: 'left',
+                                    pt: '1.5em',
+                                    fontSize: {
+                                        xs: '.9em',
+                                        sm: '1em'
+                                    },
+                                    maxWidth: '570px',
+                                    fontWeight: '300'
+                                }}
+                            >
+                                Born from the depths of cyberspace, where pixels dance with the rhythm of imagination and dreams take flight in the realm of virtual reality. With her ethereal beauty and enigmatic charm, injoi emerges as a beacon of light in a world shrouded in darkness.
+                            </Typography>
+                            <Box
+                                sx={{
+                                    my: '2.5em',
+                                    gap: '.8em',
+                                    display: 'flex',
+                                    flexWrap: 'wrap',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                {/* Discord Button */}
+                                <a href={`https://discord.com/channels/1131625905601650719/1220060290335379516/1221344390354571324`} rel="noreferrer" target='_blank'>
+                                    <Button
+                                        className="b1 explore offset"
+                                        sx={{
+                                            opacity: 0,
+                                            borderRadius: 0,
+                                            padding: '.75em 2.5em',
+                                            flex: { xs: 1, sm: 'inherit' },
+                                        }}
+                                    >
+                                        Join for Whitelist
+                                    </Button>
+                                </a>
 
+                                {/* Opensea Button */}
+                                <a href={`https://opensea.io/`} rel="noreferrer" target='_blank'>
+                                    <Button
+                                        className='b2'
+                                        sx={{
+                                            ...btnStyles,
+                                            opacity: 0,
+                                            height: 'max-content',
+                                            padding: '1em 1.5em',
+                                            width: { xs: '100%', sm: 'auto' },
+                                            ":hover": {
+                                                color: '#0092ff'
+                                            }
+                                        }}
+                                        variant='text'
+                                    >
+                                        <Typography fontSize='14px'>
+                                            View Opensea
+                                        </Typography>
+                                    </Button>
+                                </a>
                             </Box>
-
                         </Box>
-
-                    </Box>
+                    </Grid>
+                    <Grid
+                        sx={{
+                            mb: {
+                                xs: '3.5em',
+                                sm: '4em',
+                                lg: '1em'
+                            },
+                            mr: {
+                                sm: '1em',
+                                md: '2em',
+                                lg: '0em'
+                            },
+                            display: 'flex',
+                            alignItems: 'baseline',
+                            justifyContent: 'flex-end'
+                        }}
+                        item
+                        xs={12}
+                        sm={12}
+                        md={12}
+                        lg={5}
+                    >
+                        <Box
+                            sx={{
+                                borderRadius: '6px',
+                                width: '100%', // Make the box full width of its parent container
+                            }}
+                        >
+                            {/* Your content here */}
+                            <Box
+                                className='mainBox'
+                                sx={{
+                                    opacity: 0,
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    position: 'relative',
+                                    height: '460px',
+                                    boxShadow: {
+                                        xs: '.5em 3em 0 #313131 ',
+                                        sm: '2em 3em 0px #313131'
+                                    }
+                                }}
+                            >
+                                <Image
+                                    layout='fill'
+                                    style={{
+                                        opacity: 0,
+                                        borderRadius: '6px',
+                                        zIndex: '2'
+                                    }}
+                                    className='img1'
+                                    src={`/assets/injoi.gif`}
+                                    alt="injoy gif"
+                                />
+                                <Box
+                                    className='gradientBg'
+                                    sx={{
+                                        width: '100px',
+                                        height: '100px',
+                                        zIndex: '0',
+                                        position: 'absolute',
+                                        right: {
+                                            xs: '-1%',
+                                            sm: '-5%'
+                                        },
+                                        opacity: 0,
+                                        bottom: '-5%',
+                                        background: 'transparent',
+                                        backgroundImage: 'radial-gradient(white 2px, transparent 0)',
+                                        backgroundSize: '15px 13px'
+                                    }}
+                                ></Box>
+                                <Box
+                                    className='quoteBox'
+                                    sx={{
+                                        zIndex: '2',
+                                        position: 'absolute',
+                                        bottom: {
+                                            xs: '0%',
+                                            lg: '-5%'
+                                        },
+                                        width: '100%',
+                                        right: {
+                                            sm: '25%'
+                                        },
+                                        top: '105%',
+                                        overflow: 'hidden',
+                                        opacity: 0,
+                                        background: '#0092ff'
+                                    }}
+                                >
+                                    <Typography
+                                        sx={{
+                                            fontWeight: '300',
+                                            fontSize: '.85em',
+                                            padding: '1em'
+                                        }}
+                                    >
+                                        {"A free NFT mint, a digital muse, and a gateway to rewards "}
+                                        <span style={{ color: 'white' }}>
+                                            {" - By "}
+                                            <a href="https://eacnft.com/" target="_blank" rel="noopener noreferrer">
+                                                {"EAC"}
+                                            </a>
+                                        </span>
+                                    </Typography>
+                                </Box>
+                            </Box>
+                        </Box>
+                    </Grid>
                 </Grid>
+            </Container>
+        </div>
+    );
+};
 
-            </Grid>
-
-        </Container>
-    )
-}
-
-export default Hero
+export default Hero;
